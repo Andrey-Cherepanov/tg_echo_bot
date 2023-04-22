9558d9e01acc7235594912958b05c179c4c0c28b
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-04-22 11:03:19 +0500

    add photo handler

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram import F
 
 API_TOKEN = ''
 with open('token.txt', 'r') as f:
@@ -16,6 +17,10 @@ async def process_start_command(message: Message):
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Text me something, I\'ll echo your message')
+
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
 @dp.message()
 async def send_echo(message):
