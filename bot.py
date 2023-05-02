0b543ce7b3aeca4020e862b754eed0ae359b5afc
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-05-02 17:08:16 +0500

    incude routers

--- bot.py
+++ bot.py
@@ -2,12 +2,16 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config_data.config import Config, load_config
+from handlers import user_handlers, other_handlers
 
 async def main() -> None:
     config: Config = load_config()
 
     bot: Bot = Bot(token=config.tg_bot.token)
     dp: Dispatcher = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     # Пропускаем накопившиеся апдейты
     await bot.delete_webhook(drop_pending_updates=True)
