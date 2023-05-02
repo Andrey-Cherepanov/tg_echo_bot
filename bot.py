022ba329f278aeab35ad8e4decc82de5f0092ba7
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-05-02 18:46:45 +0500

    fixed polling

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ async def main() -> None:
     # Пропускаем накопившиеся апдейты
     await bot.delete_webhook(drop_pending_updates=True)
     # Запускаем поллинг
-    await dp.run_polling(bot)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
