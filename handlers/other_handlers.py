004157e6577d2ba782b00c1a9ac750693e491d4b
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-05-02 18:47:51 +0500

    fixed echo sending

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -8,6 +8,6 @@ router: Router = Router()
 @router.message()
 async def send_echo(message:Message):
     try:
-        await message.send_copy(chat_id=message.chat_id)
+        await message.send_copy(chat_id=message.chat.id)
     except:
         await message.reply(text=LEXICON_RU['no_echo'])
